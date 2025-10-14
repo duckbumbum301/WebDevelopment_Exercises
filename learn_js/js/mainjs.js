fa9fd843bb9e05448a4c32fe88434ac8117f4d56
duckbumbum <ducnc234112e@st.uel.edu.vn>
2025-10-15 00:07:14 +0700

    xcvb

---
+++ learn_js/js/mainjs.js
@@ -0,0 +1,15 @@
+function calc_factorial(n) 
+{
+    fact=1
+    for(i=1;i<=n;i++)
+    {
+        fact*=i
+    }
+    return fact
+}
+
+function use_factorial(n,result)
+{
+    fact=calc_factorial(n)
+    result.innerText=fact
+}
